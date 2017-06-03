3ebf10376074d532783a73d2b0f4e66c1b3eaea3
Will Tennien Murphy <will.t.murphy@gmail.com>
2017-06-02 22:22:33 -0400

    new

--- upload.sh
+++ upload.sh
@@ -1,4 +1,5 @@
 # from http://peterdowns.com/posts/first-time-with-pypi.html
 # increment version number
-python3 setup.py register -r pypi
+python3 setup.py sdist
+# python3 setup.py register -r pypi # this command is no longer necessary
 python3 setup.py sdist upload -r pypi
